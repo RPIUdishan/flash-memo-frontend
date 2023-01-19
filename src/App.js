afcdbca5a53de87c9fbaa5fd237f10c7ad4f38f4
RPIUdishan <it18082234@my.sliit.lk>
2023-01-19 23:16:56 +0530

    deck container ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import AddCard from "./components/add-card-deck/add-card-deck-component";
-import CardDeck from "./components/card-deck/card-deck";
+import CardDeckContainer from "./components/card-deck/card-deck-container-component";
 
 function App() {
   return (
     <div className="App">
       <div className="mx-5">
         <AddCard />
-        <CardDeck />
+        <CardDeckContainer />
       </div>
     </div>
   );
---
+++ src/components/card-deck/card-deck-container-component.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import CardDeck from './card-deck'
+
+const CardDeckContainer = () => {
+    return (
+        <>
+            <div className=''>
+                <div class="grid grid-cols-5 gap-20 m-4">
+                    <div>
+                        <CardDeck />
+                    </div>
+                    <div>
+                        <CardDeck />
+                    </div>
+                    <div>
+                        <CardDeck />
+                    </div>
+
+                    <div>
+                        <CardDeck />
+                    </div>
+                    <div>
+                        <CardDeck />
+                    </div>
+                    <div>
+                        <CardDeck />
+                    </div>
+
+                    <div>
+                        <CardDeck />
+                    </div>
+                    <div>
+                        <CardDeck />
+                    </div>
+                    <div>
+                        <CardDeck />
+                    </div>
+                    <div>
+                        <CardDeck />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CardDeckContainer
