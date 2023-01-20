80d53291cf37213fdd6412129d1d4f4ddc7cf358
RPIUdishan <it18082234@my.sliit.lk>
2023-01-20 10:54:57 +0530

    deck ui fixes

--- src/components/card-deck/card-deck-container-component.js
+++ src/components/card-deck/card-deck-container-component.js
@@ -2,42 +2,46 @@ import React from 'react'
 import CardDeck from './card-deck'
 
 const CardDeckContainer = () => {
+
+    const data = [
+        {
+            cardName: 'Test 01',
+            cardSubject: 'Mathematics',
+            cardColor: '#349eeb'
+        },
+        {
+            cardName: 'Test 02',
+            cardSubject: 'Science',
+            cardColor: '#815bf5'
+        },
+        {
+            cardName: 'Test 03',
+            cardSubject: 'Geology',
+            cardColor: '#5db068'
+        },
+        {
+            cardName: 'Test 04',
+            cardSubject: 'Information Technology',
+            cardColor: '#e8254f'
+        },
+        {
+            cardName: 'Test 05',
+            cardSubject: 'Business Science',
+            cardColor: '#fac905'
+        }
+    ]
+
     return (
         <>
             <div className=''>
                 <div className="grid grid-cols-5 gap-20 m-4">
-                    <div>
-                        <CardDeck />
-                    </div>
-                    <div>
-                        <CardDeck />
-                    </div>
-                    <div>
-                        <CardDeck />
-                    </div>
-
-                    <div>
-                        <CardDeck />
-                    </div>
-                    <div>
-                        <CardDeck />
-                    </div>
-                    <div>
-                        <CardDeck />
-                    </div>
-
-                    <div>
-                        <CardDeck />
-                    </div>
-                    <div>
-                        <CardDeck />
-                    </div>
-                    <div>
-                        <CardDeck />
-                    </div>
-                    <div>
-                        <CardDeck />
-                    </div>
+                    {
+                        data.map(cardDeck => (
+                            <div>
+                                <CardDeck cardName={cardDeck.cardName} cardSubject={cardDeck.cardSubject} cardColor={cardDeck.cardColor} />
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </>
--- src/components/card-deck/card-deck.js
+++ src/components/card-deck/card-deck.js
@@ -1,19 +1,20 @@
 import React from 'react'
 
-const CardDeck = () => {
+const CardDeck = ({ cardName, cardSubject, cardColor }) => {
+
   return (
     <>
-    <div className='w-52 h-64 bg-blue-900 rounded-lg'>
+    <div className='w-52 h-64 rounded-lg cursor-pointer' style={{backgroundColor: cardColor}}>
         <div className='w-52 h-64 rounded-bl-lg rounded-br-lg rounded-tr-lg rounded-tl-full ring-2 bg-white'>
             <div className='float-right m-2 ring-2 ring-blue-900 rounded-full p-1 bg-blue-900'>
                 <p className='text-white font-bold'>25</p>
             </div>
             <div className='flex-col pt-48 ml-2'>
                 <div>
-                    <p className='text-2xl font-semibold'>Deck Name</p>
+                    <p className='text-2xl font-semibold'>{cardName}</p>
                 </div>
                 <div>
-                    <p>Science</p>
+                    <p>{cardSubject}</p>
                 </div>
             </div>
         </div>
