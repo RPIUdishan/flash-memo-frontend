9dd305c4977611ec97a12f199c494d66d903ca95
RPIUdishan <it18082234@my.sliit.lk>
2023-01-31 22:14:46 +0530

    loading bar added to fetch data

--- src/components/card-deck/card-deck-container-component.js
+++ src/components/card-deck/card-deck-container-component.js
@@ -2,41 +2,14 @@ import React from 'react'
 import { useGetAllCardDecksQuery } from '../../features/cardDeckApiSlice'
 import CardDeck from './card-deck'
 
-const CardDeckContainer = () => {
+import HashLoader from "react-spinners/HashLoader";
 
-    const dataa = [
-        {
-            cardName: 'Test 01',
-            cardSubject: 'Mathematics',
-            cardColor: '#349eeb'
-        },
-        {
-            cardName: 'Test 02',
-            cardSubject: 'Science',
-            cardColor: '#815bf5'
-        },
-        {
-            cardName: 'Test 03',
-            cardSubject: 'Geology',
-            cardColor: '#5db068'
-        },
-        {
-            cardName: 'Test 04',
-            cardSubject: 'Information Technology',
-            cardColor: '#e8254f'
-        },
-        {
-            cardName: 'Test 05',
-            cardSubject: 'Business Science',
-            cardColor: '#fac905'
-        }
-    ]
+const CardDeckContainer = () => {
 
     const { data, error, isError, isLoading } = useGetAllCardDecksQuery();
 
     return (
         <>
-            {console.log("on the app", data)}
             <div className='container'>
                 <div className='m-4'>
                     <div>
@@ -51,11 +24,19 @@ const CardDeckContainer = () => {
                         </div>
                     </div>
                 </div>
+                {
+                    isLoading ?
+                        <div className='w-full flex justify-center items-center'>
+                            <HashLoader
+                                color="#36d7b7"
+                            />
+                        </div> : null
+                }
                 <div className="grid grid-cols-5 gap-20 m-4">
                     {
                         data && data.map((cardDeck) => (
                             <div key={cardDeck._id}>
-                                <CardDeck cardName={cardDeck.title} cardSubject={cardDeck.subject} cardColor={cardDeck.color} noOfCards={cardDeck.noOfCards}/>
+                                <CardDeck cardName={cardDeck.title} cardSubject={cardDeck.subject} cardColor={cardDeck.color} noOfCards={cardDeck.noOfCards} />
                             </div>
                         ))
                     }
