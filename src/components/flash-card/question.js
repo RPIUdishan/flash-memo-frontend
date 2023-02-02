9f12633f02557d5c9089da367e31ee970e2726d5
RPIUdishan <it18082234@my.sliit.lk>
2023-02-02 08:55:32 +0530

    question component ui

---
+++ src/components/flash-card/question.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Question = (question) => {
+  return (
+    <>
+    <div className='w-fit'>
+        <p className='text-2xl text-slate-900'>{question}</p>
+    </div>
+    </>
+  )
+}
+
+export default Question
