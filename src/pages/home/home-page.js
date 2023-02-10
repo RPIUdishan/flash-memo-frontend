561f88948475618d3474576f5cc2aaf75ca6363a
RPIUdishan <it18082234@my.sliit.lk>
2023-02-11 01:33:35 +0530

    remove add card unnecessary

--- src/pages/home/home-page.js
+++ src/pages/home/home-page.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 import AddCardDeck from "../../components/add-card-deck/add-card-deck-component";
 import CardDeckContainer from '../../components/card-deck/card-deck-container-component'
-import AddCard from '../../components/flash-card/add-card';
 
 const HomePage = () => {
     return (
         <div className="mx-5">
             <AddCardDeck />
             <CardDeckContainer />
-            <AddCard />
         </div>
     )
 }
