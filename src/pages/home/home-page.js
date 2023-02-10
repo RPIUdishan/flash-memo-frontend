e9c78e114a09c43e1542a5e298f14fddfe7d8106
RPIUdishan <it18082234@my.sliit.lk>
2023-02-11 00:50:22 +0530

    name changed

--- src/pages/home/home-page.js
+++ src/pages/home/home-page.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
-import AddCard from "../../components/add-card-deck/add-card-deck-component";
+import AddCardDeck from "../../components/add-card-deck/add-card-deck-component";
 import CardDeckContainer from '../../components/card-deck/card-deck-container-component'
+import AddCard from '../../components/flash-card/add-card';
 
 const HomePage = () => {
     return (
         <div className="mx-5">
-            <AddCard />
+            <AddCardDeck />
             <CardDeckContainer />
+            <AddCard />
         </div>
     )
 }
